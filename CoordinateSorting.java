d89307e8d2a07b50913f7d92f22c9b7b720f7401
Sam Lerner <lerner89@gmail.com>
2017-01-19 16:45:34 -0500

    Moved Listen outside of dev group

---
+++ CoordinateSorting.java
@@ -0,0 +1,22 @@
+public static double[][] closestPair(double[][] set) {
+	double minDist = 0.0;
+	double[][] nearest = new double[2][2];
+	for(int i = 0; i < set.length; i++) {
+		for(int j = 0; j < set.length; j++) {
+			if(i != j) {
+
+			}
+		}
+	}
+}
+
+public static double distance()
+
+public static void main(String[] args) {
+	double[][] coordinates = new double[4][2];
+	
+	coordinates[0] = [40.0013957276009, -83.01363207399845];
+	coordinates[1] = [40.0017886781511, -83.01372796297073];
+	coordinates[2] = [40.0018757433591, -83.01309630274773];
+	coordinates[3] = [40.0014845911259, -83.0129749327898];
+}
